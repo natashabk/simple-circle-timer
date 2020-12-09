946f6d06b7d9c03faddb5d65020692d4eceff1d1
natashabuck <natashabuck1@gmail.com>
2020-12-09 12:28:19 +0100

    Updates

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Timer from '../src';
+const App = () => (
+  <Timer />
+);
+ReactDOM.render( <App />, document.getElementById( "root" ) );
